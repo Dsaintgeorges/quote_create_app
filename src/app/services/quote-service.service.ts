018226fa481eeb08cf00fd2f11282f25c3657f02
Doryan <doryan@MacBook-Air-de-Doryan.local>
2022-04-01 00:27:13 +0200

    modify env

--- src/app/services/quote-service.service.ts
+++ src/app/services/quote-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {environment} from "../../environments/environment";
+import {environment} from "../../environments/environment.prod";
 
 const CREATE_QUOTE_URL = environment.apiUrl+'/createQuote';
 const RESULT = environment.apiUrl+'/result';
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../models/Users";
 import {LoginRequest} from "../models/LoginRequest";
-import {environment} from "../../environments/environment";
+import {environment} from "../../environments/environment.prod";
 
 const REGISTER = environment.apiUrl + 'createUser';
 const LOGIN = environment.apiUrl + 'login';
